05d9a09445b130c05921f0e59e18f7dabaf6b9c0
Spaghetti <gpitis@hotmail.fr>
2026-04-24 22:31:13 +0200

    on essaie en ssl

--- back/database/db.py
+++ back/database/db.py
@@ -3,7 +3,10 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "postgresql+psycopg://postgres:rcU80JGow37bomBI@db.nmurjtqvveijysaqyjog.supabase.co:5432/postgres"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"}
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
