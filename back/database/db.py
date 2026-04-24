c7892f147a4d8a9d091b5e9e9d011b2ed5aa03cb
Spaghetti <gpitis@hotmail.fr>
2026-04-24 22:35:28 +0200

    aaaaaaaaaa

--- back/database/db.py
+++ back/database/db.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+psycopg://postgres:rcU80JGow37bomBI@db.nmurjtqvveijysaqyjog.supabase.co:5432/postgres"
+# DATABASE_URL = "postgresql+psycopg://postgres:rcU80JGow37bomBI@db.nmurjtqvveijysaqyjog.supabase.co:5432/postgres"
+DATABASE_URL = "postgresql+psycopg://postgres.nmurjtqvveijysaqyjog:rcU80JGow37bomBI@aws-0-eu-west-1.pooler.supabase.com:6543/postgres"
 
 engine = create_engine(
     DATABASE_URL,
